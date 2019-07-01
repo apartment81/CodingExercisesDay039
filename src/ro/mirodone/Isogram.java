82fa24d1b9e28d2ed3c819f2ace977b9b379d13a
apartment81 <bogdan.mirodone@gmail.com>
2019-07-01 23:55:05 +0300

    isogram

---
+++ src/ro/mirodone/Isogram.java
@@ -0,0 +1,30 @@
+package ro.mirodone;
+
+import java.util.Arrays;
+
+public class Isogram {
+
+    boolean isIsogram(String str) {
+
+        str = str.toLowerCase();
+        int len = str.length();
+
+       // System.out.println(len);
+       // System.out.println(str);
+
+        char arr[] = str.toCharArray();
+
+        Arrays.sort(arr);
+
+/*       for (int j = 0; j<= len-1 ; j++){
+            System.out.println(arr[j]);
+        }*/
+
+        for (int i = 0; i <len - 1; i++) {
+            if (arr[i] == arr[i + 1])
+                return false;
+        }
+            return true;
+    }
+
+}
---
+++ src/ro/mirodone/Main.java
@@ -0,0 +1,18 @@
+package ro.mirodone;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Isogram isogram = new Isogram();
+
+        System.out.println(isogram.isIsogram("isIsogram"));
+        System.out.println(isogram.isIsogram("abc"));
+        System.out.println(isogram.isIsogram("Alphabet"));
+
+
+
+
+    }
+}
